da0a3ebab4099eae8996275fe66bab6b011e7e65
WilkerSebastian <wilkerafonso2005@gmail.com>
2024-10-24 23:46:51 -0400

    up: responsive canvas

--- src/components/SpriteRender.tsx
+++ src/components/SpriteRender.tsx
@@ -1,54 +1,56 @@
 import { useEffect, useRef } from "react";
 import { SpriteSheetAnalyzer } from "../core/SpriteSheetAnalyzer";
 
-export const SpriteRender = (props: {image: HTMLImageElement | null}) => {
-
+export const SpriteRender = (props: { image: HTMLImageElement | null }) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-
+    
     const image = props.image;
-  
     const canvas = canvasRef.current;
-    const ctx = canvas?.getContext('2d');
-    const padding = 10; 
-  
+    const ctx = canvas?.getContext("2d");
+    const padding = 10;
+
     if (canvas && ctx && image) {
       
       canvas.width = image.width + padding * 2;
       canvas.height = image.height + padding * 2;
-  
+
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      
+
       let k = 0;
       let l = 0;
-  
+
       for (let i = 0; i < canvas.width; i += 10) {
         for (let j = 0; j < canvas.height; j += 10) {
-          ctx.fillStyle = (k + l) % 2 ? '#D2D2D2' : '#FDFDFD';
+          ctx.fillStyle = (k + l) % 2 ? "#D2D2D2" : "#FDFDFD";
           ctx.fillRect(i, j, 10, 10);
           k++;
         }
         l++;
       }
-      
+
       ctx.drawImage(image, padding, padding, image.width, image.height);
 
       const spriteAnalyzer = new SpriteSheetAnalyzer(image);
       const sprites = spriteAnalyzer.sliceSprites();
 
-      ctx.strokeStyle = 'red';
-
-      sprites.forEach((sprite) => ctx.strokeRect(
-        sprite.x + padding, 
-        sprite.y + padding, 
-        sprite.width, 
-        sprite.height
-      ))
-
+      ctx.strokeStyle = "red";
+      sprites.forEach((sprite) =>
+        ctx.strokeRect(
+          sprite.x + padding,
+          sprite.y + padding,
+          sprite.width,
+          sprite.height
+        )
+      );
+      
     }
   }, [props.image]);
 
-  return <canvas ref={canvasRef} className="border border-white"></canvas>
-
-}
+  return (
+    <div className="flex justify-center items-center max-w-[90vw] max-h-[90vh] overflow-auto border border-gray-300 bg-gray-100">
+      <canvas ref={canvasRef} className="border border-white" />
+    </div>
+  );
+};
